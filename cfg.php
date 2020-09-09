9839d970fc7a8352c221d712d96b5baa17567105
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-09-09 14:28:59 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+// -*- Copyleft @ °✍⎯⚚͜͡SʰⁱNᵒᵇⁱ - 2020 -*-
+// -*- Config SpeedSatoshi -*-
+// -*- If you want to donate, at DOGE Wallet:
+// -*- DDsu3tCfqXbhoj1j84Ydq3qkBYNZZESC3Q
+//________________________________________________
+
+// Register at https://bit.ly/3gH22RO
+// Input your login data in here
+
+$email = "nekodoll1@mail.com_addref"; // Your email login
+$passwd = "wasd1111"; // Your password login
+
+?>
